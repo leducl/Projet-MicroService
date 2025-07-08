994fa17455e10a66685f40ba4d7a59520e6de62a
AxelPedrero1 <pedreroa@3il.fr>
2025-07-08 13:01:48 +0200

    Update main.py

--- code/app/main.py
+++ code/app/main.py
@@ -1,6 +1,6 @@
 from app import create_app
 import os
 if __name__ == "__main__":
-    port = int(os.getenv('PORT', 5002))
+    port = int(os.getenv('PORT', 5000))
     app = create_app()
     app.run(host='0.0.0.0', port=port)
